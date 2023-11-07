b9b7ca6180daf2320f7648d47f320d9febdbf482
Rustik89 <78265628+Rustik89@users.noreply.github.com>
2023-11-07 10:42:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 const resultElement = document.getElementById('result')
+const textResult = document.getElementById('txt')
 const input1 = document.getElementById('num1')
 const input2 = document.getElementById('num2')
 const submitBtn = document.getElementById('submit')
 const plusBtn = document.getElementById('plus')
 const minusBtn = document.getElementById('minus')
+const umnogBtn = document.getElementById('umnog')
+const delenieBtn = document.getElementById('delenie')
 let action = '+'
 
 plusBtn.onclick = function () {
@@ -15,13 +18,28 @@ minusBtn.onclick = function () {
     action = '-'
 }
 
+umnogBtn.onclick = function () {
+    action = 'x'
+}
+
+delenieBtn.onclick = function () {
+    action = '/'
+}
+
 
 submitBtn.onclick = function () {
     if (action == '+') {
         const sum = Number(input1.value) + Number(input2.value)
         resultElement.textContent = sum
-    }else if (action == '-') {
+    } else if (action == '-') {
         const sum = Number(input1.value) - Number(input2.value)
         resultElement.textContent = sum
+    } else if (action == 'x') {
+        const sum = Number(input1.value) * Number(input2.value)
+        resultElement.textContent = sum
+    } else if (action == '/') {
+        const sum = Number(input1.value) / Number(input2.value)
+        resultElement.textContent = sum
     }
+    textResult.innerText = 'Заебись решено!'
 }
